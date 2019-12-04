049ca361fbf33892e13a596e47ac8059dfdd1240
Gerhard Molin <gary@Gerhards-iMac.home>
2019-12-04 22:09:27 +0200

    last changed wed night

--- deploy_automation/deploy.sh
+++ deploy_automation/deploy.sh
@@ -47,8 +47,8 @@ sudo ifup enp0s3
 #Install and configure Fail2Ban
 yes "y" | sudo apt -y install fail2ban
 
-sudo rm -rf /etc/fail2ban/jail.local
-cp assets/fail2ban/jail.local /etc/fail2ban/
+#sudo rm -rf /etc/fail2ban/jail.local
+cp assets/fail2ban/jail.local /etc/fail2ban/jail.local
 
 cp assets/fail2ban/nginx-dos.conf /etc/fail2ban/filter.d
 cp assets/fail2ban/portscan.conf /etc/fail2ban/filter.d
@@ -62,13 +62,10 @@ sudo systemctl disable apt-daily.timer
 sudo systemctl disable apt-daily-upgrade.timer
 sudo systemctl disable syslog.service
 
-#Install and configure protection against ports scan
-#yes "y" | sudo 	apt-get install portsenty
-
 #Copy and set up cron scripts for updating packages and detecting crontab changes
 
-yes "y" | sudo apt -y install mailx
-yes "y" | sudo apt install mailutils
+sudo apt-get -y install mailx
+sudo apt-get -y install mailutils
 
 cp -r assets/scripts/ ~/
 ##{ crontab -e; echo '0 4 * * 7 sudo ~/update.sh'; } | crontab -e 
@@ -81,9 +78,14 @@ cp -r assets/scripts/ ~/
 
 { crontab -l -u root; echo '0 0 * * * SUN ~/scripts/monitor.sh'; } | crontab -u root -
 
+{ crontab -l -u gmolin; echo '0 4 * * SUN sudo ~/scripts/update.sh'; } | crontab -u gmolin -
+{ crontab -l -u gmolin; echo '@reboot sudo ~/scripts/update.sh'; } | crontab -u gmolin -
+
+{ crontab -l -u gmolin; echo '0 0 * * * SUN ~/scripts/monitor.sh'; } | crontab -u gmolin -
+
 #Install Apache
 
-yes "y" | sudo apt install apache2 -y
+sudo apt install apache2 -y
 sudo systemctl enable apache2
 yes "y" | rm -rf /var/www/html/
 cp -r assets/apache/ /var/www/html/
